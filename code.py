32c5bd1d8852e23b30929bbed04b03283573efff
Anushka1-Singh <9651anushka@gmail.com>
2025-12-20 12:28:44 +0530

    code.py

---
+++ code.py
@@ -0,0 +1,14 @@
+f = open("sample.txt","r") #file object
+data = f.readline()
+print(data)
+
+data = f.readline()
+print(data)
+f=open("sample.txt","w")
+f1=f.write("jjjjjjjjhhhhh")
+print(f1)
+f.close()
+
+
+
+
