61afa31a0a0cf6baf8bb3ab7273753d0c07489c2
Anushka1-Singh <9651anushka@gmail.com>
2025-12-20 14:01:12 +0530

    added folder with files

---
+++ practice problem/code.py
@@ -0,0 +1,5 @@
+f=open("practice problem/sample1.txt.","r")
+data=f.read()
+print(data)
+if data=='python':
+    print(data)
---
+++ practice problem/code1.py
@@ -0,0 +1,13 @@
+data=True
+line=1
+word="python"
+with open("practice problem/sample1.txt","r") as f:
+    while data:
+        data=f.readline()
+        if word in data:
+            print(f"word found at line {line}")
+            print(word)
+            break
+            
+        line +=1
+        
